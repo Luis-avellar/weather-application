cae9400d3829f35b2aa6d7d8137dcdd7dd44aad0
Luis-avellar <avellar.mr@gmail.com>
2023-07-13 12:24:06 -0300

    request do local e do tempo feito

---
+++ js/weather.js
@@ -0,0 +1,34 @@
+const APIKey = 'JqWXbYR8s6jvsMfjhtsyaTHhS75yvxXV'
+const getCityUrl = cityName => 
+`http://dataservice.accuweather.com/locations/v1/cities/search?&apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+  try{
+    const cityUrl = getCityUrl(cityName)
+    const response = await fetch(cityUrl)
+
+    if(!response.ok){
+      throw new Error('Não foi possível obter os dados.')
+    }
+    const [cityData] = await response.json()
+    return cityData
+  }catch({name, message}){
+    alert(`${name}: ${message}`)
+  }
+}
+const getCityWeather = async cityName => {
+  try{
+    const {Key} = await getCityData(cityName)
+    const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
+    const response = await fetch(cityWeatherUrl)
+
+    if(!response.ok){
+      throw new Error('Não foi possível obter os dados.')
+    }
+    const [cityWeatherData] = await response.json()
+    return cityWeatherData
+  }catch({name, message}){
+    alert(`${name}: ${message}`)
+  }
+}
+getCityWeather('São Paulo')
