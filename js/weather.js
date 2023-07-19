be3e946dc0b50cb7f236999ca7d037a817ea7ebf
Luis-avellar <avellar.mr@gmail.com>
2023-07-19 16:01:40 -0300

    pequena fatoracao de request

--- js/weather.js
+++ js/weather.js
@@ -2,11 +2,12 @@ const APIKey = 'BzS8XaNEMrDFADqJwgITeJSdMWhztjGs'
 const getCityUrl = cityName => 
 `http://dataservice.accuweather.com/locations/v1/cities/search?&apikey=${APIKey}&q=${cityName}`
 
+const getRequestData = async url => await fetch(url) 
 const getCityData = async cityName => {
   try{
     const cityUrl = getCityUrl(cityName)
-    const response = await fetch(cityUrl)
-
+    const response = await getRequestData(cityUrl)
+    
     if(!response.ok){
       throw new Error('Não foi possível obter os dados.')
     }
@@ -20,7 +21,7 @@ const getCityWeather = async cityName => {
   try{
     const {Key} = await getCityData(cityName)
     const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
-    const response = await fetch(cityWeatherUrl)
+    const response = await getRequestData(cityWeatherUrl)
 
     if(!response.ok){
       throw new Error('Não foi possível obter os dados.')
@@ -33,3 +34,4 @@ const getCityWeather = async cityName => {
   }
 }
 getCityWeather('Recife')
+  .then(resolve => console.log(resolve))
