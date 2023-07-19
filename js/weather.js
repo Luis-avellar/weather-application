5e19c4848b4f3945729d6fea1fa6e53a86e86b37
Luis-avellar <avellar.mr@gmail.com>
2023-07-19 11:58:18 -0300

    tentando resolver o error do fetch

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ const getCityUrl = cityName =>
 const getCityData = async cityName => {
   try{
     const cityUrl = getCityUrl(cityName)
-    const response = await fetch(cityUrl, {mode: 'no-cors',})
+    const response = await fetch(cityUrl)
 
     if(!response.ok){
       throw new Error('Não foi possível obter os dados.')
@@ -19,14 +19,14 @@ const getCityData = async cityName => {
 const getCityWeather = async cityName => {
   try{
     const {Key} = await getCityData(cityName)
-    const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
+    const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}?language=pt-br`
     const response = await fetch(cityWeatherUrl)
-    
+    console.log(Key)
     if(!response.ok){
       throw new Error('Não foi possível obter os dados.')
     }
     const [cityWeatherData] = await response.json()
-    
+
     return cityWeatherData
   }catch({name, message}){
     alert(`${name}: ${message}`)
