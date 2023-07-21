8382e2c7fa2618f73193bd68301006578e3195f6
Luis-avellar <avellar.mr@gmail.com>
2023-07-21 14:13:36 -0300

    fatoração do app que inseri as info no DOM

--- js/app.js
+++ js/app.js
@@ -7,25 +7,36 @@ const cityCard = document.querySelector('[data-js="city-card"]')
 let timeImg = document.querySelector('[data-js="time"]')
 const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
-
-cityForm.addEventListener('submit', async event => {
-  event.preventDefault()
-
-  const inputValue = event.target.city.value
+const showCityCard = () => {
+  const isDisplayNoneValid = cityCard.classList.contains('d-none')
+  
+  if(isDisplayNoneValid){
+    cityCard.classList.remove('d-none')
+  }
+}
+const showWeatherInfo = async inputValue => {
   const [{Key, LocalizedName}] = await getCityData(inputValue)
   const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await 
     getCityWeather(Key)
   const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
+  
 
   timeIconContainer.innerHTML = timeIcon
   nameContainer.textContent = LocalizedName
   weatherContainer.textContent = WeatherText
   temperatureContainer.textContent = Temperature.Metric.Value
-  
-  if(cityCard.classList.contains('d-none')){
-    cityCard.classList.remove('d-none')
-  }
-
   IsDayTime ? timeImg.src = './src/day.svg' : timeImg.src = './src/night.svg'
+}
+
+cityForm.addEventListener('submit',  event => {
+  event.preventDefault()
+
+  const inputValue = event.target.city.value
+  
+  showWeatherInfo(inputValue)
+  showCityCard()
+  
+
+  
   cityForm.reset()
 })
